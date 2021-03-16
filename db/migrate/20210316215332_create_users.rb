8166014de0c5150b697d02353819f293d63253c0
James Grieve <pocojames@hotmail.com>
2021-03-16 14:54:22 -0700

    add user model

---
+++ db/migrate/20210316215332_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.string :profile_img
+      t.string :bio
+
+      t.timestamps
+    end
+  end
+end
