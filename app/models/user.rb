5bdc85026df1fe632171ccf72386b626abcbed1b
James Grieve <pocojames@hotmail.com>
2021-03-16 15:29:43 -0700

    add unique validation to user email and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-    
-    validates :email, presence: true
-    validates :username, presence: true
+
+    validates :email, presence: true, uniqueness: true
+    validates :username, presence: true, uniqueness: true
     validates :password_digest, presence: true
 end
