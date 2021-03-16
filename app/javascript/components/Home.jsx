8568d46f56e7958c961a8ff99f9fccde7b6034d4
James Grieve <pocojames@hotmail.com>
2021-03-16 12:50:12 -0700

    rename home component to homepage

--- app/javascript/components/Home.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-export default () => (
-  <div>
-      Home
-  </div>
-);
--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "../components/Home";
+import Home from "../components/Homepage";
 
 export default (
   <Router>
     <Switch>
-      <Route path="/" exact component={Home} />
+      <Route path="/" exact component={Homepage} />
     </Switch>
   </Router>
 );
