ad877ad233fca0d734558e539ca07a2ab39542b3
James Grieve <pocojames@hotmail.com>
2021-03-16 14:01:28 -0700

    basic homepage structure complete

--- app/javascript/components/Homepage.jsx
+++ app/javascript/components/Homepage.jsx
@@ -6,7 +6,14 @@ const Homepage = () => (
   <div>
       <Navbar />
       <div className = "container">
-        <PostFeed />
+        <div className = "row">
+          <div className = "col-md-12 col-lg-9">
+            <PostFeed />
+          </div>
+          <div className = "col-3 d-none d-lg-block">
+            fsadfads
+          </div>
+        </div>
       </div>
   </div>
 );
