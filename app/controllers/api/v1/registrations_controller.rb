9c1db02ef77eb343f50515108bb2757304c0296d
James Grieve <pocojames@hotmail.com>
2021-03-17 00:27:02 -0700

    add basic create action for new user

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -3,5 +3,16 @@ class Api::V1::RegistrationsController < ApplicationController
     end
   
     def create
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user
+        else
+            render json: @user.errors
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username,:email,:password, :password_confirmation)
     end
 end
