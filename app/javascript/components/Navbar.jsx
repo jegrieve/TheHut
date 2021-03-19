02b6f41793bb0e00ca666195a1cde2e1be8d4903
James Grieve <pocojames@hotmail.com>
2021-03-18 23:42:21 -0700

    add conditional rendering in navbar for signed in user

--- app/javascript/components/Navbar.jsx
+++ app/javascript/components/Navbar.jsx
@@ -7,10 +7,13 @@ const Navbar = (props) => {
     return (
         <div className = "site-navbar d-flex justify-content-around">
             <div>Website</div>
+            {props.currentUser 
+            ? <UserOptions setCurrentUser = {props.setCurrentUser} currentUser = {props.currentUser}/>
+            : <div>
             <SignUpForm setCurrentUser = {props.setCurrentUser} currentUser = {props.currentUser} />
             <SignInForm setCurrentUser = {props.setCurrentUser} currentUser = {props.currentUser} />
-            {/* if user is signed in*/}
-            <UserOptions setCurrentUser = {props.setCurrentUser} currentUser = {props.currentUser}/>
+            </div>
+            }
         </div>
 
     )
