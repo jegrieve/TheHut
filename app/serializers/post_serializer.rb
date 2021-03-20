f649a25e6047f45ece23409802782bd1d2fa4669
James Grieve <pocojames@hotmail.com>
2021-03-20 00:47:17 -0700

    config post serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,11 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id
+  include Rails.application.routes.url_helpers
+  attributes :id, :title, :body, :image
+  def image
+    if object.image.attached?
+      {
+        url: rails_blob_url(object.image)
+      }
+    end
+  end
 end
