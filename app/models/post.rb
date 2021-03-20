eb853f68b3c042977b776be45d5488cc42d882ea
James Grieve <pocojames@hotmail.com>
2021-03-20 01:18:05 -0700

    add associations between post and user models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
     has_one_attached :image
+    belongs_to :user
+
+    validates :title, presence: true
 end
--- db/migrate/20210319234556_create_posts.rb
+++ db/migrate/20210319234556_create_posts.rb
@@ -1,8 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.string :title
+      t.string :title, null: false
       t.text :body
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
