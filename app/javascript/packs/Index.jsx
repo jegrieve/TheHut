fbad977ee2dc49f63dda0cf0aea0791fb6593ff8
James Grieve <pocojames@hotmail.com>
2021-03-24 16:03:10 -0700

    add react-loader-spinner css

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from "../routes/App";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 
 document.addEventListener("DOMContentLoaded", () => {
