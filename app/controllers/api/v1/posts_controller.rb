1cf9b173c2954bd6c0b63ee1dc9653f91ec6caa6
James Grieve <pocojames@hotmail.com>
2021-03-23 23:20:56 -0700

    add show action in posts controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -3,6 +3,16 @@ class Api::V1::PostsController < ApplicationController
         @posts = Post.limit(params[:limit]).offset(params[:offset])
         render json: @posts
     end
+
+    def show
+        @post = Post.find(params[:id])
+        if post
+            render json: @post
+        else
+            render json: @post.errors
+        end
+    end
+
     def create
         user = User.find_by(id: session[:user_id])
         @post = user.posts.create(post_params)
