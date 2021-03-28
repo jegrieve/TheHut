25ea9dc36d374114c671c415d8fe6876dad60161
James Grieve <pocojames@hotmail.com>
2021-03-28 15:57:29 -0700

    add reference to user in board migration

--- db/migrate/20210328070334_create_boards.rb
+++ db/migrate/20210328070334_create_boards.rb
@@ -3,6 +3,7 @@ class CreateBoards < ActiveRecord::Migration[6.1]
     create_table :boards do |t|
       t.string :title, null: false
       t.text :body, null: false
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -46,8 +46,10 @@ ActiveRecord::Schema.define(version: 2021_03_28_070334) do
   create_table "boards", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_boards_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -81,6 +83,7 @@ ActiveRecord::Schema.define(version: 2021_03_28_070334) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "boards", "users"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
