38fe3da3f46060f7fea0c07c48e8e39ff51893ab
James Grieve <pocojames@hotmail.com>
2021-03-28 00:07:24 -0700

    add title and body tables to board migration

--- db/migrate/20210328070334_create_boards.rb
+++ db/migrate/20210328070334_create_boards.rb
@@ -1,6 +1,8 @@
 class CreateBoards < ActiveRecord::Migration[6.1]
   def change
     create_table :boards do |t|
+      t.string :title, null: false
+      t.text :body, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_25_071019) do
+ActiveRecord::Schema.define(version: 2021_03_28_070334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 2021_03_25_071019) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "boards", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "comments", force: :cascade do |t|
