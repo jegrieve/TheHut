4fd22fe4142447b2be26f1eda1f255817764bbc1
James Grieve <pocojames@hotmail.com>
2021-03-28 15:58:11 -0700

    add association between user model and board model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
     has_secure_password
     has_many :posts
     has_many :comments
+    has_many :boards
 
     validates :email, presence: true, uniqueness: true
     validates :username, presence: true, uniqueness: true
