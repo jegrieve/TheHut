083beb1e1b36e0cb7235fb53eb904986b7cc571f
James Grieve <pocojames@hotmail.com>
2021-03-28 13:15:56 -0700

    add board controller index,create actions and params

--- app/controllers/api/v1/boards_controller.rb
+++ app/controllers/api/v1/boards_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::BoardsController < ApplicationController
+    def index
+        @boards = Board.all
+        render json: @boards
+    end
+    def create
+        @board = Board.create(board_params)
+    end
+    private
+    def board_params
+        params.permit(:title, :body, :board_image)
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,3 +5,4 @@ class Post < ApplicationRecord
 
     validates :title, presence: true
 end
+
