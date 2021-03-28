70100ef314fa22e2e3381350e4992cda42f023cb
James Grieve <pocojames@hotmail.com>
2021-03-28 15:47:21 -0700

    add show action to boards controller

--- app/controllers/api/v1/boards_controller.rb
+++ app/controllers/api/v1/boards_controller.rb
@@ -3,8 +3,18 @@ class Api::V1::BoardsController < ApplicationController
         @boards = Board.all
         render json: @boards
     end
+
     def create
         @board = Board.create(board_params)
+    end
+
+    def show
+        @board = Board.find(params[:id])
+        if @board
+            render json: @board
+        else
+            render json: @board.errors
+        end
     end
     private
     def board_params
