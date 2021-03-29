57028daf66a3ac5dde9651aced7258e8401cfd9d
James Grieve <pocojames@hotmail.com>
2021-03-29 01:46:21 -0700

    add association between post and board

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
     has_one_attached :image
     belongs_to :user
     has_many :comments
-    #belongs_to :board
+    belongs_to :board
 
     validates :title, presence: true
 end
