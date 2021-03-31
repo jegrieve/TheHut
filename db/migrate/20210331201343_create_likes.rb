563126b0b361845013715607e43acf49d4e9685b
James Grieve <pocojames@hotmail.com>
2021-03-31 13:19:15 -0700

    fix like model references

--- db/migrate/20210331201343_create_likes.rb
+++ db/migrate/20210331201343_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[6.1]
   def change
     create_table :likes do |t|
-      add_reference :posts, :board, null: false, foreign_key: true
-      add_reference :users, :board, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+      t.references :post, null: false, foreign_key: true
 
       t.timestamps
     end
