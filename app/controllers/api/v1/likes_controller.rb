523f2f0f262c32417f2304f7ee1e06d6b5737d18
James Grieve <pocojames@hotmail.com>
2021-03-31 14:21:25 -0700

    add create action in likes controller

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::LikesController < ApplicationController
+    def index
+
+    end
+
+    def create
+        user = User.find_by(id: session[:user_id])
+        @like = user.likes.new(post_id: params[:post_id])
+        @like.save
+        render json: @like
+    end
 end
