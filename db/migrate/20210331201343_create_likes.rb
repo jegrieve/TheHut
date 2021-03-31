2dd816d5645871bd3d32522f8a161f9567017b80
James Grieve <pocojames@hotmail.com>
2021-03-31 13:16:54 -0700

    add tables to like model

--- db/migrate/20210331201343_create_likes.rb
+++ db/migrate/20210331201343_create_likes.rb
@@ -1,6 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[6.1]
   def change
     create_table :likes do |t|
+      add_reference :posts, :board, null: false, foreign_key: true
+      add_reference :users, :board, null: false, foreign_key: true
 
       t.timestamps
     end
