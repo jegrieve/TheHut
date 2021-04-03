8cc6786c7726b0689dfcf696c2886f7ceb355cb3
James Grieve <pocojames@hotmail.com>
2021-04-02 23:22:13 -0700

    structured the navbar elements

--- app/javascript/components/Navbar.jsx
+++ app/javascript/components/Navbar.jsx
@@ -6,17 +6,27 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = (props) => {
     return (
-        <div className = "site-navbar d-flex justify-content-around">
+        <div className = "site-navbar d-flex justify-content-around align-items-center">
             <div>Website</div>
             {props.currentUser 
-            ? <div>
-                <UserOptions setCurrentUser = {props.setCurrentUser} currentUser = {props.currentUser}/>
-                <NavLink to="/create-post">Create Post (+)</NavLink>
-                <NavLink to={`/user/${props.currentUser.id}`}>Current User</NavLink>
+            ? <div className = "d-flex navbar-user-controls justify-content-between">
+                <div>
+                    <NavLink to="/create-post">Create Post (+)</NavLink>
+                </div>
+                <div>
+                    <NavLink to={`/user/${props.currentUser.id}`}>Current User</NavLink>
+                </div>
+                <div>
+                    <UserOptions setCurrentUser = {props.setCurrentUser} currentUser = {props.currentUser}/>
+                </div>
               </div>
-            : <div>
-            <SignUpForm setCurrentUser = {props.setCurrentUser} currentUser = {props.currentUser} />
-            <SignInForm setCurrentUser = {props.setCurrentUser} currentUser = {props.currentUser} />
+            : <div className = "d-flex navbar-user-controls justify-content-between">
+                <div>
+                    <SignUpForm setCurrentUser = {props.setCurrentUser} currentUser = {props.currentUser} />
+                </div>
+                <div>
+                    <SignInForm setCurrentUser = {props.setCurrentUser} currentUser = {props.currentUser} />
+                </div>
             </div>
             }
         </div>
