401973ac60788394584965fa0ba33ac2a9709ada
James Grieve <pocojames@hotmail.com>
2021-04-02 23:58:24 -0700

    add current_user method to get current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+    def current_user
+        return unless session[:user_id]
+        @current_user ||= User.find(session[:user_id])
+      end
 end
