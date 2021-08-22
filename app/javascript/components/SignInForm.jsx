0c1980820ff454475fdda68f39e8d88d94ad3c6e
James Grieve <pocojames@hotmail.com>
2021-08-21 20:59:17 -0700

    fix structuring of signinform and change css colours to grey background

--- app/javascript/components/SignInForm.jsx
+++ app/javascript/components/SignInForm.jsx
@@ -1,4 +1,6 @@
 import React, {useState} from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 const SignInForm = (props) => {
     const [signInUserInputs, setSignInUserInputs] = useState({
@@ -66,17 +68,21 @@ const SignInForm = (props) => {
             <button className = "btn btn-secondary" onClick = {bringUpSignInForm}>Log In</button>
             <div className = "sign-in-form d-none clickable">
                 <form className = "sign-in-inputs">
-                    <button onClick = {exitSignInForm} type="button" className="close close-btn-container" aria-label="Close">
-                        <span className="close-btn" aria-hidden="true">&times;</span>
-                    </button>
-                    <div className = "form-group">
-                        <label>Username:
+                    <div>
+                        <span className = "sign-in-title">Login</span>
+                        <span onClick = {exitSignInForm} type="button" className="close close-btn-container" aria-label="Close">
+                            <FontAwesomeIcon icon={faTimes} />
+                            {/* <span className="close-btn" aria-hidden="true">&times;</span> */}
+                        </span>
+                    </div>
+                    <div className = "form-group sign-in-username-input">
+                        <label className = "sign-in-label">Username:
                             <small id="signin-usernameHelp" className="form-text red-text"></small>
                             <input id="sign-in-user-username" name = "username" type="text" className = "form-control" onChange = {enterSignInInputs} value = {signInUserInputs["username"]}/>
                          </label>
                     </div>
-                    <div className = "form-group">
-                        <label>Password:
+                    <div className = "form-group sign-in-username-password">
+                        <label className = "sign-in-label">Password:
                         <input id="sign-in-user-password" name = "password" className = "form-control" type="password" onChange = {enterSignInInputs} value = {signInUserInputs["password"]}/>
                     </label>
                     </div>
