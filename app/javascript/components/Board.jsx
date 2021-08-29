07769b30e91fc5eaa817e0e83e4c222a05dc4610
James Grieve <pocojames@hotmail.com>
2021-08-29 15:45:03 -0700

    create Board component for boardData

---
+++ app/javascript/components/Board.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Board = (props) => {
+    return (
+        <div>Board Data Stuff</div>
+    )
+}
+
+
+export default Board;
