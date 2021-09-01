a7b018f439332668fa4f53518285eaac19bf03ec
James Grieve <pocojames@hotmail.com>
2021-09-01 02:36:56 -0700

    remove time from created_at attribute

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,6 @@ class Comment < ApplicationRecord
     belongs_to :post
 
     def created_at
-        attributes['created_at'].strftime("%b %d %Y at %l:%M%P")
+        attributes['created_at'].strftime("%b %d %Y")
     end
 end
