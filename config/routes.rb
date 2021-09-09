f1ab944801bd34eaf8acede93cbfb7bd649a4742
James Grieve <pocojames@hotmail.com>
2021-09-09 02:42:12 -0700

    add update route for posts

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
       get "posts/index", to: "posts#index"
       post "posts/create", to: 'posts#create'
       get 'posts/show/:id', to: 'posts#show'
+      patch 'posts/update/:id', to: 'posts#update'
 
       get "comments/index", to: "comments#index"
       post "comments/create/:id", to: 'comments#create'
