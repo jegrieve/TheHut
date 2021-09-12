e4ddd58362003dfd114a8390f998ca015bfd8ca2
James Grieve <pocojames@hotmail.com>
2021-09-11 23:38:16 -0700

    add edit submission function for board edit

--- app/javascript/components/Board.jsx
+++ app/javascript/components/Board.jsx
@@ -1,28 +1,113 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { NavLink } from "react-router-dom";
 
 const Board = (props) => {
+    const [editBoardData, setEditBoardData] = useState(null);
+    const [editBoard, setEditBoard] = useState(false);
+    const [confirmDelete, setConfirmDelete] = useState(false);
     console.log(props.data)
-    return (
-        <div>
-            <div className = "container">
-                <div className = "row">
-                    <div className = "col-sm-12 col-lg-8">
-                        <div className = "show-board-title">{props.data.title}</div>
-                        <div className = "board-create-data">Created by <NavLink className = "board-user-link" to={`/user/${props.data.user_id.id}`}>u/{props.data.user_id.username}</NavLink> • {props.data.created_at} </div>
-                        <div className = "show-board-body">{props.data.body}</div>
+
+    useEffect(() => {
+        setEditBoardData({...props.data})
+    }, [])
+
+    const confirmEditBoardData = () => {
+        setEditBoard("text");
+    }
+
+    const cancelEditBoard = () => {
+        setEditBoardData({...props.data})
+        setEditBoard(false);
+    }
+
+    const deleteBoard = () => {
+        setConfirmDelete(true);
+    }
+
+    const cancelDeleteBoard = () => {
+        setConfirmDelete(false);
+    }
+
+    const confirmDeleteBoard = () => {
+        //delete props stuff here.
+    }
+
+    const submitEditBoardData = (e) => {
+        e.preventDefault();
+        props.submitEditBoardData(editBoardData.id, editBoardData);
+        setEditBoard(false);
+    }
+
+    const handleEditBoard = (e) => {
+        setEditBoardData((prev) => ({
+          ...prev,
+          [e.target.name]: e.target.value
+        }));
+      }
+
+      if (editBoard === "text"
+        && props.currentUser
+        && props.currentUser.id === props.data.user_id.id) {
+            return (
+            <div>
+                <form onSubmit = {submitEditBoardData}>
+                    <div className = "form-group">
+                        <div>Name</div>
+                        <input className = "form-control" name = "title" type = "text" onChange = {handleEditBoard} value = {editBoardData["title"]} minLength = {4} maxLength = {21} placeholder = "Title required" required/>
                     </div>
-                    <div className = "col-4 d-none d-lg-block">
-                        {props.data.board_image ? 
-                        <div>
-                            <img className = "show-board-img" src = {props.data.board_image.url} />
-                        </div> 
-                        : false}
+                    <div className = "form-group">
+                        <div>Body</div>
+                        <textarea className = "form-control" name = "body" type = "text" onChange = {handleEditBoard} value = {editBoardData["body"]} rows = {4} placeholder = "Describe the type of content found in this board" minLength = {6} maxLength = {100} required/>
+                    </div>
+                    <div className = "edit-btns">
+                        <button type = "submit" className = "btn btn-success submit-btn">Submit</button>
+                        <button className = "btn btn-danger cancel-btn" onClick = {cancelEditBoard}>Cancel</button>
+                    </div>
+                </form>
+            </div>)
+        } else if (editBoard === "image"
+        && props.currentUser
+        && props.currentUser.id === props.data.user_id.id)  {
+            return (
+                <div>
+                    <form>
+
+                    </form>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <div className = "container">
+                        <div className = "row">
+                            <div className = "col-sm-12 col-lg-8">
+                                <div className = "show-board-title">{props.data.title}</div>
+                                <div className = "board-create-data">Created by&nbsp; 
+                                    <NavLink className = "board-user-link" to={`/user/${props.data.user_id.id}`}>u/{props.data.user_id.username}</NavLink>
+                                    &nbsp;• {props.data.created_at} 
+                                </div>
+                                {props.currentUser 
+                                && props.currentUser.id === props.data.user_id.id ?
+                                <div>
+                                    <span onClick = {confirmEditBoardData}>Edit Board </span>
+                                    • <span onClick = {deleteBoard}>Delete Board</span>
+                                </div>: false}
+                                <div className = "show-board-body">
+                                    {props.data.body}
+                                </div>
+                            </div>
+                            <div className = "col-4 d-none d-lg-block">
+                                {props.data.board_image ? 
+                                <div>
+                                    <img className = "show-board-img" src = {props.data.board_image.url} />
+                                </div> 
+                                : false}
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    )
+            )
+        }
 }
 
 
--- app/javascript/components/ShowBoard.jsx
+++ app/javascript/components/ShowBoard.jsx
@@ -29,13 +29,40 @@ const ShowBoard = (props) => {
           })
           .catch(() => console.log("error"));
     }
+
+    const submitEditBoardData = (boardId, editBoardData) => {
+      const body = {
+        title: editBoardData["title"],
+        body: editBoardData["body"]
+    }
+      const url = `/api/v1/boards/update/${boardId}`;
+      const token = document.querySelector('meta[name="csrf-token"]').content;
+      fetch(url, {
+      method: "PATCH",
+      headers: {
+      "X-CSRF-Token": token, 
+      "Content-Type": "application/json"
+    },
+      body: JSON.stringify(body)
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json()
+        }
+        throw new Error("Network response was not ok.");
+    })
+    .then(response => {
+        getBoardData();
+    })
+    .catch(error => console.log(error.message))
+    }
     
     return (
       <div className = "boardpage">
         <div className = "boardpage-container container">
           <div className = "row">
             <div className = "col-md-12 col-lg-12">
-              {boardData ? <Board data = {boardData} /> : false}
+              {boardData ? <Board data = {boardData} currentUser = {props.currentUser} submitEditBoardData = {submitEditBoardData} /> : false}
             </div>
           </div>
           <hr></hr>
