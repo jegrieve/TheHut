32afb66d82995ba877648d38691bb4378eecf86f
James Grieve <pocojames@hotmail.com>
2021-09-13 00:55:46 -0700

    add CreateReply component to app

---
+++ app/javascript/components/CreateReply.jsx
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from "react";
+
+const CreateReply = (props) => { 
+   
+
+    return (
+        <div>
+            Create Reply
+        </div>
+    )
+}
+
+export default CreateReply;
