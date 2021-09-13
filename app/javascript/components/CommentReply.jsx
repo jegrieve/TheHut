8648d215513d7f7b59e387e3ffa482a805e7708a
James Grieve <pocojames@hotmail.com>
2021-09-12 19:24:27 -0700

    create commentReply component

---
+++ app/javascript/components/CommentReply.jsx
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from "react";
+
+const CommentReply = (props) => { 
+ 
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default CommentReply;
