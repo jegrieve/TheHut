5dd333f7b008f2057888e3f6aaf8835c7dfee1d2
James Grieve <pocojames@hotmail.com>
2021-09-15 20:08:36 -0700

    change file locations

--- app/javascript/components/Navbar/Navbar.jsx
+++ app/javascript/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SignUpForm from "./SignUpForm";
 import SignInForm from "./SignInForm";
-import UserOptions from "../UserOptions";
+import UserOptions from "./UserOptions";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPen, faUser } from '@fortawesome/free-solid-svg-icons'
