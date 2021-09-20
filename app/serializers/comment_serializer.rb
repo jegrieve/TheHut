94bc2a44abba98f25ac444e7b6a0ec8619191f2b
James Grieve <pocojames@hotmail.com>
2021-09-20 01:47:00 -0700

    add user serializer to comment/reply serializers

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,3 +1,4 @@
 class CommentSerializer < ActiveModel::Serializer
+  belongs_to :user, :serializer => UserSerializer
   attributes :id, :body, :user, :created_at, :replies
 end
--- app/serializers/reply_serializer.rb
+++ app/serializers/reply_serializer.rb
@@ -1,3 +1,4 @@
 class ReplySerializer < ActiveModel::Serializer
+  belongs_to :user, :serializer => UserSerializer
   attributes :id, :body, :created_at, :user
 end
