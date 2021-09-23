52c2b80e794bd6728ecd532456a22f2474ac696d
James Grieve <pocojames@hotmail.com>
2021-09-23 01:40:48 -0700

    update user show action, render last_post/board

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,13 @@ class Api::V1::UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     if user
+      if params[:post]
+        render json: {last_post: user.posts.last}
+      elsif params[:board]
+        render json: {last_board: user.boards.last}
+      else
         render json: user
+      end
     else
         render json: user.errors
     end
