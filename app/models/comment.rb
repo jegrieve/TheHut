c9634ce8e1981e87285d977e1f54506b56b69ec4
James Grieve <pocojames@hotmail.com>
2021-09-23 00:00:04 -0700

    add counter cache to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
     belongs_to :user
-    belongs_to :post
+    belongs_to :post, :counter_cache => true
     has_many :replies, dependent: :destroy
 
     def created_at
