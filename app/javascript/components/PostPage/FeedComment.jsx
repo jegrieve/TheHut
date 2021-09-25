eaf431799b8abb3feed800778f920d89f43f91ec
James Grieve <pocojames@hotmail.com>
2021-09-24 19:24:55 -0700

    log3

--- app/javascript/components/PostPage/FeedComment.jsx
+++ app/javascript/components/PostPage/FeedComment.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
-import { useEffect, useState } from "react/cjs/react.development";
+import { useEffect, useState } from "react";
 import ReplyFeed from "./ReplyFeed";
 
 const FeedComment = (props) => { 
